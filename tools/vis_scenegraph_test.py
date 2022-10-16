7618be13aed8b41c097273e01ebfdc0c2fc1e67a
Dongmin Shin <newdm2000@gmail.com>
2022-10-16 19:12:50 +0900

    integration of relation about same object-subject

--- tools/vis_scenegraph_test.py
+++ tools/vis_scenegraph_test.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--obj_path', default='/home/dongmin/Downloads/3DSSG/objects.json')
 parser.add_argument('--rel_path', default='/home/dongmin/Downloads/3DSSG/relationships.json')
-parser.add_argument('--scene_num', default=2)
+parser.add_argument('--scene_num', default=4)
 
 args = parser.parse_args()
 
@@ -44,7 +44,7 @@ for relationship_num in range(len(relationships["scans"])):
         break
 
 print(scan_id)
-
+print(json.dumps(relationships["scans"][scan_rel_num]["relationships"], indent="\t") )
 #networkx setup
 G = nx.Graph()
 dg = graphviz.Digraph()
@@ -54,7 +54,6 @@ pale_rgb = [112,191,64]
 pale_hex = webcolors.rgb_to_hex(pale_rgb)
 tomato_hex = webcolors.rgb_to_hex(tomato_rgb)
 blue_hex = webcolors.rgb_to_hex(blue_rgb)
-
 #import object node
 for object_num in range(len(objects["scans"][scene_num]["objects"])):
     object = objects["scans"][scene_num]["objects"][object_num]
@@ -76,25 +75,24 @@ for object_num in range(len(objects["scans"][scene_num]["objects"])):
                     fillcolor= blue_hex, fontcolor = 'black')
             dg.edge(("objects_" + str(object["id"])), ("objects_" + str(object["id"]) + attribute))
 
-#relation preprocessing
-sorted_relationship = {}
+#import relation node
 for relationship_num in range(len(relationships["scans"][scan_rel_num]["relationships"])):
     relationship = relationships["scans"][scan_rel_num]["relationships"][relationship_num]
-    ob_sub = (relationship[0], relationship[1])
-    if ob_sub in sorted_relationship:
-        sorted_relationship[ob_sub].append(relationship[3])
-    else:
-        sorted_relationship[ob_sub] = [relationship[3]]
-
-#import relation node
-for index, object in enumerate(sorted_relationship.keys()):
+    #sorting 후 선 하나로 나타내기
     #relation class
-    node_name = '\n'.join(sorted_relationship[object])
-    dg.node(("relationship_" + node_name + str(index)), shape='box', style='filled,rounded', 
-            label= (node_name),
-            margin= '0.11, 0.0001', width='0.11', height='0',
-            fillcolor= pale_hex, fontcolor = 'black')
-    dg.edge(("objects_" + str(object[0])), ("relationship_" + node_name + str(index)))
-    dg.edge(("relationship_" + node_name + str(index)), ("objects_" + str(object[1])))
+    if relationship[2] < 27 or relationship[2] > 32:
+        """dg.node(("relationship_" + str(relationship_num)), shape='box', style='filled,rounded',
+                label= (relationship[3]),
+                margin= '0.11, 0.0001', width='0.11', height='0',
+                fillcolor= pale_hex, fontcolor = 'black')
+        dg.edge(("objects_" + str(relationship[0])), ("relationship_" + str(relationship_num)))
+        dg.edge(("relationship_" + str(relationship_num)), ("objects_" + str(relationship[1])))"""
+        dg.edge(("objects_" + str(relationship[0])), ("objects_" + str(relationship[1])))
+
 
 dg.render("/home/dongmin/Scene-Graph-Dataset-Generator/results/test", view=True)
+
+
+
+
+  
